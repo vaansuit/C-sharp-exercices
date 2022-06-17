662bd53cfde8ddec5b84b070c6c0215e0561f5c5
Victor Hugo Vansuit <33068334+vaansuit@users.noreply.github.com>
2022-06-17 00:13:12 -0300

    basic commit

---
+++ Exercises/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RetangularBox
+{
+    class Box
+    {
+        [STAThread]
+
+        static void Main(string[] args)
+    {
+            double v, c, l, a;
+
+            Console.Write("Please enter the length of the box in meters: ");
+            c = double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the width of the box in meters: ");
+            l = double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the height of the box in meters: ");
+            a = double.Parse(Console.ReadLine());
+
+            v = c * a * l;
+
+            Console.WriteLine("The volume is: " + v);
+
+
+
+        }
+    }
+}
+
+
+
+
+
